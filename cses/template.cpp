ac03b4eea80be3d4279909cd9fd7af3acb17da09
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-08-26 13:19:26 +0700

    Update template.cpp

--- cses/template.cpp
+++ cses/template.cpp
@@ -15,17 +15,23 @@
 #define ll long long
 #define pii pair<int , int>
 #define pll pair<long long, long long>
+#define INTMAX 2147483647
+#define INTMIN -2147483647
 
 using namespace std;
 
 int t;
 int n;
+int k;
 string s;
+
+
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
 
 
     return 0;

36c747c8720d9bfa2413bf46762e907cd53a7fe4
truongnhatanh7 <truongnhatanhw@gmail.com>
2022-08-26 14:25:05 +0700

    update

---
+++ cp_training_2022/l3/template.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+#include <set>
+#include <unordered_map>
+#include <map>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <list>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <cmath>
+
+#define ll long long
+#define pii pair<int , int>
+#define pll pair<long long, long long>
+#define INTMAX 2147483647
+#define INTMIN -2147483647
+
+using namespace std;
+
+int t;
+int n;
+int k;
+string s;
+
+
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+
+
+
+    return 0;
+}
